ab1ac066b5a250a7505374c0134b25c89c645be1
jtrenovation <w.dunn.9876@gmail.com>
2026-03-26 16:07:27 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='jt-locked-layout-final-v1';const ASSETS=['./','./index.html','./manifest.json','./logo.png','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
